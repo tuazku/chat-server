a137016787d3fa8f6e98af56bf986fd8fe5bcb03
tuazku <tuazku@Cool-VAIO>
2012-05-18 06:57:15 +0600

    Building up simple server with GUI - Finished

---
+++ chat/src/Server.java
@@ -0,0 +1,152 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+/**
+ * @author Azamat Turgunbaev
+ *
+ */
+public class Server extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JTextField enterField;
+	private JTextArea displayArea;
+	
+	private ObjectOutputStream outputStream;
+	private ObjectInputStream inputStream;
+	
+	private ServerSocket server;
+	private Socket connection;
+	
+	private int counter = 1;
+	
+	public Server() {
+		
+		super("Server Application with GUI");
+		
+		enterField = new JTextField();
+		enterField.setEditable(false);
+		enterField.addActionListener(
+			new ActionListener() {
+				
+				public void actionPerformed( ActionEvent event ) {
+					
+					sendMessage( event.getActionCommand() );
+					enterField.setText("");
+				}
+			}
+		);
+		
+		add( enterField, BorderLayout.NORTH);
+		
+		displayArea = new JTextArea();
+		
+		add( new JScrollPane( displayArea ));
+		
+		setSize( 300, 500 );
+		setVisible( true );
+	}
+	
+	public void runServer() {
+		
+		try {
+			
+			server = new ServerSocket(1234, 10);
+		
+			while( true ) {
+			
+				try {
+					waitConnection();
+					getStreams();
+					processConnection();
+				} 
+				catch ( EOFException e) {
+					displayMessage("\nServer terminated connection");
+				}
+				finally {
+					closeConnection();
+					++counter;
+				}
+				
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void waitConnection() throws IOException {
+		displayMessage("Waiting for connection\n");
+		connection = server.accept();
+		displayMessage("Connection " + counter + " received from host " + connection.getInetAddress().getHostAddress());
+	}
+	
+	private void getStreams() throws IOException {
+		outputStream = new ObjectOutputStream( connection.getOutputStream() );
+		outputStream.flush();
+		
+		inputStream = new ObjectInputStream( connection.getInputStream() );
+	}
+	
+	private void processConnection() throws IOException {
+		String message = "Connection Successful";
+		sendMessage( message );
+		
+		enterField.setEditable( true );
+		
+		do {
+			try {
+				message = (String)inputStream.readObject();
+				displayMessage( message );
+			}
+			catch ( ClassNotFoundException e ) {
+				displayMessage("\nUnknown object type received");
+			}
+		} while ( !message.equals( "CLIENT>>> TERMINATE" ));
+	}
+	
+	private void closeConnection() {
+		displayMessage( "\nTerminating connection\n");
+		enterField.setEditable(false);
+		
+		try {
+			outputStream.close();
+			inputStream.close();
+			connection.close();
+		} 
+		catch( IOException e ) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void sendMessage( String message ) {
+		
+		try {
+			outputStream.writeObject( "SERVER>>> " + message );
+			outputStream.flush();
+			displayMessage( "\nSERVER>>> " + message );
+		}
+		catch (IOException e) {
+			displayArea.append( "Error writing Object" );
+		}
+	}
+	
+	private void displayMessage( String message ) {
+		displayArea.append( message );	
+	}
+}
---
+++ chat/src/ServerTest.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+/**
+ * @author Azamat Turgunbaev
+ *
+ */
+public class ServerTest{
+
+	public static void main( String [] args ) {
+		
+		Server serverApplication = new Server();
+		serverApplication.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		serverApplication.setVisible( true );
+		serverApplication.setSize(300, 350);
+		serverApplication.runServer();
+	}
+}

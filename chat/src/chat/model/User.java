cd74ef1f3f3c3377ed4e83d30dc5a085baf1bae7
Azamat Turgunbaev <Azamat Turgunbaev@Cool-VAIO>
2012-05-20 15:03:57 +0600

    Configuring User model for Hibernate use

--- chat/src/chat/model/User.java
+++ chat/src/chat/model/User.java
@@ -1,9 +1,61 @@
 package chat.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author Azamat Turgunbaev
  *
  */
+
+@Entity
+@Table(name="user")
 public class User {
 
+	private int id;
+	private String name;
+	private String password;
+	private boolean online;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@Column(name="id")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Column(name="name")
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name="password")
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Column(name="online")
+	public boolean isOnline() {
+		return online;
+	}
+
+	public void setOnline(boolean online) {
+		this.online = online;
+	}
 }
